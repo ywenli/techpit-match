de6fa7cead4d7bb51e8a720e1b37a96d524c4f08
ywenli <kuota0204@gmail.com>
2020-08-11 21:39:48 +0900

    Update action: subscribed, unsubscribed, speak, render_message #19

--- app/channels/chat_room_channel.rb
+++ app/channels/chat_room_channel.rb
@@ -1,12 +1,21 @@
 class ChatRoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "chat_room_#{params:[:room_id]}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    message = ChatMessage.create(chat_room_id: data['room_id'], user_id: data['user_id'], message: data['message'])
+    ChatRoomChannel.broadcast_to "chat_room_#{data['room_id']}", content: render_message(message)
   end
+
+  private
+
+    def render_message(message)
+      ApplicationController.renderer.render(partial: 'chat_messages/chat_message', locals: { chat_message: message })
+    end
+
 end
