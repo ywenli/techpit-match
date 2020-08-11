04df17545492e46035cb0ae223f988edd9181722
ywenli <kuota0204@gmail.com>
2020-08-11 10:07:38 +0900

    Generate uploader file #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  mount_uploader :img_name, ImgNameUploader
 end
