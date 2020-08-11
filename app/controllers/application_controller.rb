3c615cc532e951d3b6c7837d1c2c8a5c85dd8fa4
ywenli <kuota0204@gmail.com>
2020-08-11 12:07:17 +0900

    Add action: after_sign_in_path_for #9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction sex img_name))
   end
 
+  # ログイン後スワイプ画面に遷移
+  def after_sign_in_path_for(resource)
+    users_path
+  end
+
 end
