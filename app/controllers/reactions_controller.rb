c4b9077ed5d975810e2d42f3956f01ad2273a950
ywenli <kuota0204@gmail.com>
2020-08-11 14:54:32 +0900

    Add action: create #11

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,2 +1,10 @@
 class ReactionsController < ApplicationController
+
+  def create
+    # find_or_initialize_byメソッド: 引数で渡したカラムでfindをかけて該当しなかったらインスタンスを作成
+    reaction = Reaction.find_or_initialize_by(to_user_id: params[:user_id], from_user_id: params[:current_user.id])
+    reaction.update_attributes(
+      status: params[:reaction]
+    )
+  end
 end
