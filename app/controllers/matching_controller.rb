793580d16d83233900bf994c9fd2c821d77d2950
ywenli <kuota0204@gmail.com>
2020-08-11 15:59:47 +0900

    Add action: index in matching_controller #13

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -1,2 +1,8 @@
 class MatchingController < ApplicationController
+
+  def index
+    # 自分にいいねをした人のuser_idを取得
+    got_reaction_user_ids = Reaction.where(to_user_id: current_user.id, status: 'like').pluck(:from_user_id)
+    @match_users = Reaction.where(to_user_id: got_reaction_user_ids, from_user_id: current_user.id, status: 'like').map(&:to_user)
+  end
 end
