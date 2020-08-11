655ad3cfbd25bebcfa716c793666621381887100
ywenli <kuota0204@gmail.com>
2020-08-11 10:08:15 +0900

    Add mount_uploader in user.rb #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  mount_uploader :img_name, ImgNameUploader
 end
