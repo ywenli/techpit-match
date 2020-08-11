2b13dbcc7664b365a1fb67ef1e5aec58eb746eee
ywenli <kuota0204@gmail.com>
2020-08-11 15:29:43 +0900

    Fix typo #11

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -2,7 +2,7 @@ class ReactionsController < ApplicationController
 
   def create
     # find_or_initialize_byメソッド: 引数で渡したカラムでfindをかけて該当しなかったらインスタンスを作成
-    reaction = Reaction.find_or_initialize_by(to_user_id: params[:user_id], from_user_id: params[:current_user.id])
+    reaction = Reaction.find_or_initialize_by(to_user_id: params[:user_id], from_user_id: current_user.id)
     reaction.update_attributes(
       status: params[:reaction]
     )
