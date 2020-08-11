95e25b9ae3665606ca7883a72b995d62670daab4
ywenli <kuota0204@gmail.com>
2020-08-11 15:40:42 +0900

    Add routing: index for matching #13

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'top#index'
 
   resources :users, only: %i(index show)
-
   resources :reactions, only: %i(create)
+  resources :matching, only: %i(index)
 end
